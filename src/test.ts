12de2e48d9c677d10ad0cd891f9fd24ab43c2d90
rama4zis <rama.4ditya3@gmail.com>
2023-02-19 02:16:19 +0700

    update test

--- src/test.ts
+++ src/test.ts
@@ -1,46 +1,67 @@
 import * as fs from 'fs';
 
-const filename = './data/multiple.json';
+// const filename = './data/multiple.json';
+const studentsFile = './data/student.json';
+const detailStudentsFile = './data/detailStudent.json';
 
-interface fileStudent {
-  name: string;
+interface detailStudentType {
   japaneseName: string;
-  japaneseReading: string;
-  school: string;
-  age: string;
+  japaneseReadings: string;
+  age: number;
   birthday: string;
   height: string;
   hobbies: string;
-  voicedActor: string;
+  voiceActor: string;
   releaseDate: string;
 }
 
-fs.readFile(filename, 'utf8', (err, data) => {
-  if (err) {
-      console.error(err);
-      return;
+
+interface studentType {
+  name: string;
+  rarity: number;
+  isLimited: boolean;
+  school: string;
+  role: string;
+  position: string;
+  attackType: string;
+  armorType: string;
+  combatClass: string;
+  weaponType: string;
+  usesCover: boolean;
+
+  detailStudent: detailStudentType;
+
+  urban: string;
+  outdoor: string;
+  indoor: string;
+
+  image: string;
+}
+
+const data1 = fs.readFileSync(studentsFile, 'utf8');
+const data2 = fs.readFileSync(detailStudentsFile, 'utf8');
+
+let rawStudents: studentType[] = JSON.parse(data1);
+const rawDetailStudents: detailStudentType[] = JSON.parse(data2);
+
+for (let i = 0; i < rawStudents.length; i++) {
+  rawStudents[i].detailStudent = rawDetailStudents[i];
+}
+
+// make folder with name 
+for (const student of rawStudents) {
+  const folderName = student.name.toLowerCase().split(' ').join('-');
+
+  // make directory from the name 
+  // if exsit, replace it
+  if (fs.existsSync(`./assets/data/students/${folderName}`)) {
+    fs.rmdirSync(`./assets/data/students/${folderName}`, { recursive: true });
   }
+  fs.mkdirSync(`./assets/data/students/${folderName}`);
 
-  const students: fileStudent[] = JSON.parse(data);
-
-  // make folder with name 
-  for (const student of students) {
-      const folderName = student.name.toLowerCase().split(' ').join('-');
-
-      // make directory from the name 
-      fs.mkdirSync(`./assets/data/students/${folderName}`);
-
-      // make file data.json
-      fs.writeFileSync(`./assets/data/students/${folderName}/data.json`, JSON.stringify(student));
+  // make file data.json
+  if (fs.existsSync(`./assets/data/students/${folderName}/data.json`)) {
+    fs.unlinkSync(`./assets/data/students/${folderName}/data.json`);
   }
-
-  const filterStrudents = students.map((student) => {
-      return {
-          name: student.name,
-          school: student.school,
-      }
-  });
-
-  console.log(filterStrudents);
-
-});
+  fs.writeFileSync(`./assets/data/students/${folderName}/data.json`, JSON.stringify(student, null, 2));
+}
