1d7316026b9f20ea544fb1b3184dc0a0caf3dabf
rama4zis <rama.4ditya3@gmail.com>
2023-02-19 17:36:39 +0700

    Disable edit and delete

--- src/app/Student/Route.ts
+++ src/app/Student/Route.ts
@@ -4,8 +4,10 @@ import studentController from './Controller';
 const router = Router();
 const controller = new studentController();
 
-router.route('/student').get(controller.getStudents).post(controller.addStudent);
+router.route('/student').get(controller.getStudents)
+// router.route('/student').post(controller.addStudent);
 router.route('/student/search').get(controller.searchStudent);
-router.route('/student/:id').get(controller.getStudent).put(controller.updateStudent).delete(controller.deleteStudent);
+router.route('/student/:id').get(controller.getStudent);
+// router.route('/student/:id').put(controller.updateStudent).delete(controller.deleteStudent);
 
 export default router;
