bd5e34a0c1140c14528602abc5541f99b014bf68
rama4zis <rama.4ditya3@gmail.com>
2023-02-19 23:23:11 +0700

    add root path message

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Response } from 'express';
 import studentRouter from './app/Student/Route';
 import notFound from './middleware/404';
 import errorHandler from './middleware/errorHandler';
@@ -8,6 +9,13 @@ const app = express();
 const port = process.env.PORT || 3000; // default port to listen
 
 app.use(express.json());
+
+// home route 
+app.get('/', (_req, res: Response): void => {
+    res.status(200).json({
+        message: 'Blue Archive Companion API'
+    });
+});
 
 app.use('/api/v1', studentRouter);
 
